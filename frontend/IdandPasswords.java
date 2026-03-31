5d2d27ab0b28554b444ad1b14984dc1e4da60662
Alfredo Gonzalez <freddy_1904@icloud.com>
2026-03-30 20:18:15 -0700

    Added Log in page and user is able to type

---
+++ frontend/IdandPasswords.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+
+public class IdandPasswords{
+    HashMap<String, String> loginInfo= new HashMap<String, String>();
+
+protected HashMap<String, String> getloginInfo(){
+    return loginInfo;
+}
+
+}
