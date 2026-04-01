12ec7cdccf33587ac55aa9057095b58c55e79ed9
Daniel Mentado <daniel_mentado@Daniels-MacBook-Air-98.local>
2026-04-01 15:23:13 -0700

    Added MealPlanPanel and updated HomePage with dynamic navbar

--- frontend/loginapp/src/main/java/com/loginapp/Login.java
+++ frontend/loginapp/src/main/java/com/loginapp/Login.java
@@ -1,11 +1,16 @@
 package com.loginapp;
 
-import java.util.HashMap;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.*;
+import java.util.HashMap;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
 public class Login implements ActionListener {
     JFrame frame = new JFrame();
